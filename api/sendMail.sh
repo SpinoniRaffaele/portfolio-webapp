ceb6132bdcb46a978e97a82815181ecfd3c6579f
SpinoniRaffaele <raffaele.spinoni@gmail.com>
2022-09-07 15:10:00 +0200

    parametrize the call

--- api/sendMail.sh
+++ api/sendMail.sh
@@ -1,3 +1,26 @@
+import "querystring@1.3.0"
+
 handler() {
-    curl -s -X POST --user "${API_KEY}:e8e9d3bd0a79e4ad9d5944bf25c2180e" https://api.mailjet.com/v3.1/send -H "Content-Type:application/json" -d '{"Messages":[{"From": {"Email": "raffaele.spinoni@gmail.com","Name": "Raffaele"},"To": [{"Email": "raffaelespinoni@gmail.com","Name": "Raffaele"}],"Subject": "My first Mailjet email","TextPart": "Greetings from Mailjet.","HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!","CustomID": "AppGettingStartedTest"}]}'
+	local path
+	local QUERY
+    local ADDRESS
+    local BODY
+	path="$(jq -r '.path' < "$1")"
+	QUERY="$(querystring "$path")"
+
+    IFS="&"
+    set -- $QUERY
+    IFS="="
+    set -- $1
+    ADDRESS=$2
+    echo $ADDRESS
+
+    IFS="&"
+    set -- $QUERY
+    IFS="="
+    set -- $2
+    BODY=$2
+    echo $BODY
+
+    curl -s -X POST --user "${API_KEY}:${API_SECRET}" https://api.mailjet.com/v3.1/send -H "Content-Type:application/json" -d '{"Messages":[{"From": {"Email": "'${ADDRESS}'","Name": "Raffaele"},"To": [{"Email": "raffaelespinoni@gmail.com","Name": "Raffaele"}],"Subject": "Mail from the portfolio website","TextPart": "'${BODY}'","HTMLPart": "<br>","CustomID": "AppGettingStartedTest"}]}'
 }
