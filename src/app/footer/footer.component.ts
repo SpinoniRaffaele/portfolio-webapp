a8f101f7e0bbf73d2ec8d3836ddd7aeed40605dd
SpinoniRaffaele <raffaele.spinoni@gmail.com>
2022-09-22 11:24:51 +0200

    add the source code link in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  readonly sourceCodeLink = "https://github.com/SpinoniRaffaele/portfolio-webapp";
+
   constructor() { }
 
   ngOnInit(): void {
