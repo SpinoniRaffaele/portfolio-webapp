dd2b9ddaa16d003da34c2382d6193952ebf0b5cf
SpinoniRaffaele <raffaele.spinoni@gmail.com>
2022-09-22 15:45:19 +0200

    update content

--- src/app/about-me-content/about-me-content.datamodel.ts
+++ src/app/about-me-content/about-me-content.datamodel.ts
@@ -5,6 +5,9 @@ const mediumText: string = 'I\'ve recently begun a side hustle as a technical wr
     + 'I write mainly about technologies and Computer Science, but not only: as a passionate reader, ' 
     + 'somatimes I publish content related to books that i\'ve recently read. If you like the content remember to subscribe!'
 
+const developerText: string = 'I have studied computer science in Politecnico di Milano in Italy obtaining a Bachelor\'s ' +
+    'Degree in 2019 and a Master Degree in 2022. I am now working fulltime as a fullstack developer at Amadeus. ' +
+    'I\ve worked with quite some technologies as a dev and i\'m always looking forward learn some new ones.'
 
 export const aboutMeContent: ContentInfo[][] =
 [
@@ -13,12 +16,12 @@ export const aboutMeContent: ContentInfo[][] =
 
         {
             imagePath: '../../../assets/images/AngularLogo.png',
-            text: 'Test Angular'
+            text: developerText
         },
 
         {
             imagePath: '../../../assets/images/vercelLogo.png',
-            text: 'Test Vercel'
+            text: 'Test Verc'
         },
 
         {
@@ -34,6 +37,18 @@ export const aboutMeContent: ContentInfo[][] =
         }
     ],
 // third group - Sports
+    [
+        {
+            imagePath: '../../../assets/images/vercelLogo.png',
+            text: 'Test Vercel'
+        },
+
+        {
+            imagePath: '',
+            text: 'Test No Image'
+        }
+    ],
+// fourth group - languages - not sure
     [
         {
             imagePath: '../../../assets/images/vercelLogo.png',
