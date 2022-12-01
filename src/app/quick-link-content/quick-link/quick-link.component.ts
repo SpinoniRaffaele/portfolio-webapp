18e8676021eeeff1178829ce25e7df8526f21fcd
SpinoniRaffaele <raffaelespinoni@gmail.com>
2022-12-01 17:01:28 +0100

    add quick links animation

--- src/app/quick-link-content/quick-link/quick-link.component.ts
+++ src/app/quick-link-content/quick-link/quick-link.component.ts
@@ -1,19 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input } from '@angular/core';
 import { ImageLoaderService } from 'src/app/shared/image-loader.service';
+import { halfHeaderMenuTransitionTime } from 'src/app/shared/transition-timing.datamodel';
 import { QuickLink } from './quick-link-datamodel';
 
 @Component({
   selector: 'app-quick-link',
   templateUrl: './quick-link.component.html',
-  styleUrls: ['./quick-link.component.scss']
+  styleUrls: ['./quick-link.component.scss'],
+  animations:  [
+    trigger('animationTrigger', [
+      state('normal', style({'transform': 'scale(1)'})),
+      state('expanded', style({'transform': 'scale(1.2)'})), 
+      transition('normal => expanded', [
+        animate(halfHeaderMenuTransitionTime, style({'transform': 'scale(1.3)'})),
+        animate(halfHeaderMenuTransitionTime)
+      ]),
+      transition('expanded => normal', [
+        animate(halfHeaderMenuTransitionTime, style({'transform': 'scale(0.9)'})),
+        animate(halfHeaderMenuTransitionTime)
+      ]),
+    ])
+  ]
 })
-export class QuickLinkComponent implements OnInit {
+export class QuickLinkComponent {
+
+  animationStatus: 'normal' | 'expanded' = 'normal';
 
   @Input() source: QuickLink = {link: '', imagePath: ''};
 
   constructor(public imageLoader: ImageLoaderService) { }
 
-  ngOnInit(): void {
+  pulseIn() {
+    this.animationStatus = 'expanded';
   }
 
+  pulseOut() {
+    this.animationStatus = 'normal';
+  }
 }
--- src/app/shared/transition-timing.datamodel.ts
+++ src/app/shared/transition-timing.datamodel.ts
@@ -1,2 +1,3 @@
 export const headerMenuTransitionTime = '300ms';
+export const halfHeaderMenuTransitionTime = '150ms';
 export const contentFadeInTransitionTime = '1s'
